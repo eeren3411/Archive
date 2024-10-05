c3f159304a1e23169aeb6c503cc1fb77388e0afd
eeren3411 <eeren3411@gmail.com>
2024-10-05 03:13:38 +0300

    Initialize express for backend server

--- CompanyDatabase/server.js
+++ CompanyDatabase/server.js
@@ -1,1 +1,11 @@
-console.log("Hello World!");
+const PORT = 3411
+
+const express = require('express');
+const app = express();
+
+const router = require('./routers/main');
+app.use(router);
+
+app.listen(PORT, () => {
+    console.log(`Server listening on port: ${PORT}`);
+})
