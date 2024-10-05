78be2ee9c6533a4e537c2f1dded43938bb7fc2a6
Eeren <eeren3411@gmail.com>
2024-10-05 04:07:50 +0300

    CompanyDatabase add dynamic router support (#3)

--- CompanyDatabase/server.js
+++ CompanyDatabase/server.js
@@ -3,8 +3,8 @@ const PORT = 3411
 const express = require('express');
 const app = express();
 
-const router = require('./routers/main');
-app.use(router);
+const routeHelpers = require('./helpers/routeHelpers');
+routeHelpers.ImportRoutes(app, './routers')
 
 app.listen(PORT, () => {
     console.log(`Server listening on port: ${PORT}`);
