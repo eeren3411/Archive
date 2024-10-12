3b71dc70d50ec91115e891002dceeba603fdddfa
eeren3411 <eeren3411@gmail.com>
2024-10-13 00:14:15 +0300

    Initialize DataManagerInstance with server start, handle exits

--- CompanyDatabase/Server.js
+++ CompanyDatabase/Server.js
@@ -1,3 +1,39 @@
+import { DataManagerInstance } from '#managers/DataManager';
+/**
+ * Function to call before shutdown
+ * @param {string} signal 
+ * @param {any} reason 
+ * @param {any} promise 
+ */
+function BeforeShutdown(signal, reason, promise) {
+    console.log(`Received signal: ${signal}. Closing the application.`);
+    if (reason && promise) {
+        console.log(`Unhandled promise rejection at: ${promise}, reason: ${reason}`);
+    } else if (reason) {
+        console.log(`Uncaught exception: ${reason}`);
+    }
+
+    try {
+        DataManagerInstance.Dispose();
+    } catch (err){
+        console.log(`Error occured database connection dispose: ${err}`);
+    } finally {
+        process.exit(0);
+    }
+}
+
+['SIGINT', 'SIGTERM', 'SIGUSR1', 'SIGUSR2'].forEach(signal => {
+    process.on(signal, () => BeforeShutdown(signal));
+});
+
+process.on('uncaughtException', (err) => {
+    BeforeShutdown('uncaughtException', err);
+});
+
+process.on('unhandledRejection', (reason, promise) => {
+    BeforeShutdown('unhandledRejection', reason, promise);
+});
+
 import express from 'express';
 const app = express();
 
