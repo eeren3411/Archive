572b949b0971a30cf62dbbd1b0234ba0b51d978a
eeren3411 <eeren3411@gmail.com>
2024-10-13 18:37:54 +0300

    Extend and rename session middleware

---
+++ CompanyDatabase/middleware/SessionMW.js
@@ -0,0 +1,31 @@
+import { SessionManagerInstance } from '#managers/SessionManager';
+import { StatusCodes } from 'http-status-codes';
+import { SESSION_TIMEOUT } from '#config';
+
+/**
+ * 
+ * @param {import('express').Request} req 
+ * @param {import('express').Response} res 
+ * @param {import('express').NextFunction} next 
+ */
+export function SessionValidatorMW(req, res, next) {
+    const result = SessionManagerInstance.Validate(req);
+
+    if (!result) return res.status(StatusCodes.UNAUTHORIZED).send({error: "Invalid Session"});
+
+    res.cookie('sessionid', req.header['sessionid'], {
+        httpOnly: true,
+        sameSite: 'strict',
+        maxAge: SESSION_TIMEOUT
+    });
+    next();
+}
+
+export function CreateSessionMW(req, res, next) {
+    res.cookie('sessionid', SessionManagerInstance.CreateSession(req), {
+        httpOnly: true,
+        sameSite: 'strict',
+        maxAge: SESSION_TIMEOUT
+    });
+    next();
+}
